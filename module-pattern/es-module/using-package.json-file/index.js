80637b19d1a3e02d83b6d3fb6936881be14ebbe2
arvindpndit <arvindpandit9808@gmail.com>
2024-05-22 15:49:21 +0530

    es module pattern

---
+++ module-pattern/es-module/using-package.json-file/index.js
@@ -0,0 +1,6 @@
+//const calculation = require("./calculation");
+import calculation from "./calculation.js";
+
+const { add, sub, mul, div } = calculation;
+console.log(add(2, 5));
+console.log(sub(2, 5));
